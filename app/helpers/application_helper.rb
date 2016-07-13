880263b20c5a2029a943645f52978028b8b0bd7b
rcasseb <rcasseb@gmail.com>
2016-07-13 16:19:59 -0300

    navedação por breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def build_breadcrumb(paths)
+    breadcrumbs = [{ title: "Inicio", link: root_path }]
+    breadcrumbs += paths
+
+    render "compartilhado/breadcrumb", breadcrumbs: breadcrumbs
+  end
+
 end
