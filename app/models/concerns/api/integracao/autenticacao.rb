33369d0bcd52e916097a534e49a9551eaf6e4f5d
Paulo Igor <piagodinho@gmail.com>
2016-07-14 15:49:53 -0300

    mudança no tratamento do método de autenticação do usuário

--- app/models/concerns/api/integracao/autenticacao.rb
+++ app/models/concerns/api/integracao/autenticacao.rb
@@ -3,11 +3,7 @@ module API
     module Autenticacao
       def autenticar(params)
         begin
-          response = RestClient.post "#{API::Base::URL_BASE}/autenticacao", login: params[:nome_usuario],
-                                                                senha: params[:senha]
-          json = JSON.parse(response.body)
-
-          self.new json["usuario"]
+          self.new autenticar_usuario(params)
         rescue RestClient::Unauthorized
           usuario = self.new
           usuario.errors = ActiveModel::Errors.new(usuario)
@@ -17,6 +13,14 @@ module API
         rescue
           raise "Falha na comunicação com a API"
         end
+      end
+
+      def autenticar_usuario(params)
+        response = RestClient.post "#{API::Base::URL_BASE}/autenticacao", login: params[:nome_usuario],
+                                                                senha: params[:senha]
+        json = JSON.parse(response.body)
+
+        json["usuario"]
       end
     end
   end
