60ede55252859e269ca1df346fec6aa4e0242df5
Paulo Igor <piagodinho@gmail.com>
2016-07-14 15:49:53 -0300

    mudança no tratamento do método de autenticação do usuário

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -7,37 +7,31 @@ describe Usuario do
 
   it 'valid authentication' do
     retorno_api = {"id": 1, "nome": "Administrador", "nome_usuario": "admin", "senha": "123456"}
+    expect(Usuario).to receive(:autenticar_usuario)
+                       .with(usuario_valido)
+                       .and_return(retorno_api)
 
-    expect_any_instance_of(Usuario).to receive(:request)
-                                             .with(:autenticacao, usuario_valido)
-                                             .and_return(retorno_api)
-    usuario = Usuario.new usuario_valido
-    expect(usuario.login).to be true
-    expect(usuario.nome).to eq retorno_api["nome"]
+    usuario = Usuario.login(usuario_valido)
+    expect(usuario.valid?).to be true
+    expect(usuario.nome).to eq retorno_api[:nome]
   end
 
   context 'invalid authentication' do
-    let(:usuario) { Usuario.new usuario_invalido }
-
     before do
-      expect_any_instance_of(Usuario).to receive(:request).at_least(:once).times
-                                             .with(:autenticacao, usuario_invalido)
-                                             .and_return(nil)
+      expect(Usuario).to receive(:autenticar_usuario).at_least(:once).times
+                         .with(usuario_invalido)
+                         .and_raise(RestClient::Unauthorized)
     end
 
     it {
-      expect(usuario.login).to be false
+      expect(Usuario.login(usuario_invalido).valid?).to be false
     }
 
     it 'messages' do
-      usuario.login
+      usuario = Usuario.login(usuario_invalido)
       expect(usuario.errors.messages).to_not be_empty
       expect(usuario.errors.messages[:nome_usuario].first).to eq "Nome de usuário ou Senha inválido"
       expect(usuario.errors.messages[:senha].first).to eq "Nome de usuário ou Senha inválido"
-    end
-
-    it 'exception message' do
-      expect {usuario.login!(nil)}.to raise_error("Autenticação Inválida")
     end
   end
 end
