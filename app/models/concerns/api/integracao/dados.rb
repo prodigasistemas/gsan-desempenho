1f25435bc496f38dbfb451e94e4c62bb1bde8e3a
Mateus Linhares <mateuslinhares@gmail.com>
2016-07-15 14:59:56 -0300

    extrai a logica de comunicacao com a api para outro module e criei as funcoes update e destroy

--- app/models/concerns/api/integracao/dados.rb
+++ app/models/concerns/api/integracao/dados.rb
@@ -1,57 +1,75 @@
 module API
   module Integracao
     module Dados
-      def create(params={})
+      extend ActiveSupport::Concern
+      include API::Integracao::Requisicao
+
+      module ClassMethods
+        include API::Integracao::Requisicao
+
+        def create(params={})
+          begin
+            resource_params = {}
+            resource_params[self::RESOURCE_NAME] = params
+            json = post_request [], resource_params
+            self.new json["entidade"]
+          rescue RestClient::UnprocessableEntity => e
+            entidade = self.new
+            entidade.errors = ActiveModel::Errors.new(entidade)
+            entidade
+          end
+        end
+
+        def all
+          begin
+            json = get_request
+            entidades = json["entidades"]
+            entidades.map {|entidade| self.new entidade }
+          rescue RestClient::ResourceNotFound
+            []
+          end
+        end
+
+        def find(id)
+          begin
+            json = get_request([id])
+            self.new json["entidade"]
+          rescue RestClient::ResourceNotFound
+            nil
+          end
+        end
+      end
+
+      def update(params={})
+        return unless self.id
+
         begin
           resource_params = {}
-          resource_params[self::RESOURCE_NAME] = params
-          json = post_request [], resource_params
+          resource_params[self.class::RESOURCE_NAME] = self.attributes
+          json = put_request [self.id], resource_params
           self.new json["entidade"]
         rescue RestClient::UnprocessableEntity => e
           entidade = self.new
           entidade.errors = ActiveModel::Errors.new(entidade)
           entidade
+        rescue RestClient::ResourceNotFound => e
+          false
         end
       end
 
-      def all
-        begin
-          json = get_request
-          entidades = json["entidades"]
-          entidades.map {|entidade| self.new entidade }
-        rescue RestClient::ResourceNotFound
-          []
-        end
-      end
+      def destroy
+        return unless self.id
 
-      def find(id)
         begin
-          json = get_request([id])
+          json = delete_request [self.id]
           self.new json["entidade"]
-        rescue RestClient::ResourceNotFound
-          nil
+        rescue RestClient::UnprocessableEntity => e
+          entidade = self.new
+          entidade.errors = ActiveModel::Errors.new(entidade)
+          entidade
+        rescue RestClient::ResourceNotFound => e
+          false
         end
-      end
-
-      def get_request(path=[])
-        response = RestClient.get build_url(path)
-        JSON.parse(response.body)
-      end
-
-      def post_request(path=[], params={})
-        response = RestClient.post build_url(path), params
-        JSON.parse(response.body)
-      end
-
-      private
-
-      def build_url(path)
-        resource = self.pluralize_resource_name || self::RESOURCE_NAME
-        url_base = "#{API::Base::URL_BASE}/#{resource}"
-
-        path.each { |item| url_base.concat "/#{item}" }
-
-        url_base
       end
     end
   end
