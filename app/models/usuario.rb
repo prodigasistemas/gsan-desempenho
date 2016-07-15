a1a0b0a54e202794537f88b660fbb170702b115d
Mateus Linhares <mateuslinhares@gmail.com>
2016-07-15 15:00:25 -0300

    alterei a inclusao dos modules e criei um metodo attributes global

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,9 +1,15 @@
 class Usuario < API::Model
   extend API::Integracao::Autenticacao
 
+  RESOURCE_NAME = "usuario"
+
   attr_accessor :id, :nome, :nome_usuario, :senha
 
   def self.login(credenciais={})
     autenticar(credenciais)
+  end
+
+  def self.pluralize_resource_name
+    "usuarios"
   end
 end
