2729d09600dffce78ac2a4946d5b21606d5a8bd5
Paulo Moura <paulociecomp@gmail.com>
2016-08-24 16:06:54 -0300

    layout do menu com dados do banco

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -13,4 +13,17 @@ $(function(){
     $(".sidebar").hide("slide", {direction: "left"})
   });
 
+
+  /* Menu global */
+
+  $(".panel-modulo").click(function(event) {
+    
+    // alert( $(this).data("modulo-id") )
+    var moduloId = $(this).data("modulo-id");
+
+    $(".row-submenu").hide();
+    $(".row-submenu[data-modulo-id='" + moduloId + "']").show();
+
+  });
+
 })
