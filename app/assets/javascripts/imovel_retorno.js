f6a652ef514582839d10f08c3e9f57a1f41fd252
Paulo Moura <paulociecomp@gmail.com>
2016-08-25 11:48:19 -0300

    alteracao do metodo de gerar arquivo para gerar de forma assincrona

--- app/assets/javascripts/imovel_retorno.js
+++ app/assets/javascripts/imovel_retorno.js
@@ -1,8 +1,14 @@
 $(function(){
+
+  var nomeArquivo = "",
+      intervalId = 0,
+      $btn = null;
   
   $("#btn-gerar-arquivo").click(function(event) {
 
     event.preventDefault();
+
+    $btn = $(this).button('loading');
     
     $.ajax({
       url: BASE_URL + '/arquivo_recadastramento',
@@ -10,8 +16,19 @@ $(function(){
       dataType: 'json',
       data: { usuario_id: $("#usuario_id").val() },
     })
-    .done(function() {
-      console.log("success");
+    .done(function(response) {
+      if (!response.success){
+        $(".container-arquivo").html('<div class="alert alert-danger" role="alert">'+ response.message +'</div>')
+        $btn.button('reset');
+        return;
+      }
+
+      nomeArquivo = response.nome_arquivo;
+
+      intervalId = setInterval(function(){
+        poolingVerificaSeArquivoExiste(nomeArquivo);
+      }, 10000);
+
     })
     .fail(function() {
       console.log("error");
@@ -23,4 +40,28 @@ $(function(){
 
   });
 
-})
+  function poolingVerificaSeArquivoExiste(nomeArquivo){
+    $.ajax({
+      url: BASE_URL + '/verifica_arquivo_recadastramento',
+      type: 'get',
+      dataType: 'json',
+      data: { nome_arquivo: nomeArquivo },
+    })
+    .done(function(response) {
+        
+      if (response.success){
+        window.location = BASE_URL + "/arquivo_recadastramento.csv?nome_arquivo=" + nomeArquivo;
+        clearInterval(intervalId);
+        $btn.button('reset');
+      }
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  }
+
+});
