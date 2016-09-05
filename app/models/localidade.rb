194a5141e770fc31ec1f81c75bd331fc7052286b
Paulo Igor <piagodinho@gmail.com>
2016-09-05 11:54:37 -0300

    adicionando novos campos

--- app/models/localidade.rb
+++ app/models/localidade.rb
@@ -1,3 +1,3 @@
 class Localidade < API::Model
-  attr_accessor :id, :nome, :gerencia_regional_id
+  attr_accessor :id, :nome, :gerencia_regional_id, :localidade_escritorio_local, :unidade_negocio_id
 end
