1584a92b5ad43b797f5a0112e9471fd9210da9cc
Paulo Moura <paulociecomp@gmail.com>
2016-09-06 10:56:00 -0300

    retirada de código não utilizado

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -13,17 +13,4 @@ $(function(){
     $(".sidebar").hide("slide", {direction: "left"})
   });
 
-
-  /* Menu global */
-
-  $(".panel-modulo").click(function(event) {
-    
-    // alert( $(this).data("modulo-id") )
-    var moduloId = $(this).data("modulo-id");
-
-    $(".row-submenu").hide();
-    $(".row-submenu[data-modulo-id='" + moduloId + "']").show();
-
-  });
-
 })
