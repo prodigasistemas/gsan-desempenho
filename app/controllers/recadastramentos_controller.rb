5530e56896fa094cef122d96a74c1ffa19f3b698
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-07-16 11:23:47 -0300

    Tornando os campos periodo e agente cadastral obrigatorios para pesquisa

--- app/controllers/recadastramentos_controller.rb
+++ app/controllers/recadastramentos_controller.rb
@@ -3,19 +3,23 @@ class RecadastramentosController < ApplicationController
   helper  SmartListing::Helper
 
   before_action :find_leituristas
+  before_action :verificar_parametros_obrigatorios, only: [:index]
   before_action :find_atualizacao_cadastral, :find_imovel, only: [:show]
 
   def index
     atualizacao_cadastrais = []
     @filtro = FiltroRecadastramento.new(params)
     if params[:query].present?
+      unless flash[:error].nil?
+        redirect_to recadastramentos_path
+        return
+      end
       params[:query][:empresa_id] = 50 #usuario_logado.empresa_id
       atualizacao_cadastrais = AtualizacaoCadastral.where(params[:query])
     end
     @atualizacao_cadastrais = smart_listing_create :atualizacao_cadastrais,
                                                    atualizacao_cadastrais,
-                                                   partial: 'list',
-                                                   default_sort: { leiturista_id: "desc" }
+                                                   partial: 'list'
   end
 
   def show
@@ -37,5 +41,12 @@ class RecadastramentosController < ApplicationController
 
     def find_imovel
       # @imovel = Imovel.where(id: @atualizacao_cadastral.codigo_imovel, page: 1, per_page: 10).try(:first) unless @atualizacao_cadastral.nil?
+    end
+
+    def verificar_parametros_obrigatorios
+      if params[:query].present?
+        flash[:error] = "Período Inicial e Final são obrigatórios" if params[:query][:periodo_inicial].blank? or params[:query][:periodo_final].blank?
+        flash[:error] = "Agente Cadastral é obrigatório" if params[:query][:agente_cadastral].blank?
+      end
     end
 end
