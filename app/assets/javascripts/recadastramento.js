1b907b4c16c6c2a61e96ba826c56646228b315d3
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-08-08 11:29:19 -0300

    Movendo logica de carregamento de localidade para recadastramento.js e enviando o codigo da rota ao inves do id

--- app/assets/javascripts/recadastramento.js
+++ app/assets/javascripts/recadastramento.js
@@ -3,6 +3,98 @@ $(function(){
   $('#query_periodo_final').mask('00/00/0000');
 
   var cache = {},
+  $localidade_id_inicial = $("#localidade_id_inicial"),
+  $setor_comercial_inicial = $("#setor_comercial_inicial"),
+  $setor_comercial_id_inicial = $("#setor_comercial_id_inicial"),
+  $rota_inicial = $("#rota_id_inicial");
+
+  if ( $localidade_id_inicial.val() !== "" ){
+    $setor_comercial_inicial.attr('disabled', false);
+  }
+
+  if( $setor_comercial_id_inicial.length && $setor_comercial_id_inicial.val() !== "" ) {
+    $rota_inicial.attr('disabled', false);
+
+    carregarRotasInicial(function(){
+      var rota_selecionada = $rota_inicial.data("rota-selecionada");
+      if ( rota_selecionada ) $rota_inicial.val( rota_selecionada );
+    });
+  }
+
+  $( "#localidade_inicial" ).autocomplete({
+    minLength: 2,
+    source: function( request, response ) {
+      var term = request.term;
+      if ( term in cache ) {
+        response( cache[ term ] );
+        return;
+      }
+
+      request.tipo = "localidade";
+      request.filtros = { termo: term }
+
+      $.getJSON( BASE_URL + "/filtros", request, function( data, status, xhr ) {
+        var result = $.map(data.entidades, function (value, key) {
+          return {
+            id: value.id,
+            label: value.nome,
+            value: value.nome
+          };
+        });
+
+        cache[ term ] = result;
+        response( result );
+      });
+    },
+    select: function(event, ui){
+      $localidade_id_inicial.val(ui.item.id);
+      $setor_comercial_inicial.attr("disabled", false);
+    }
+  });
+
+  $setor_comercial_inicial.autocomplete({
+    minLength: 2,
+    source: function( request, response ) {
+      var term = request.term;
+      if ( term in cache ) {
+        response( cache[ term ] );
+        return;
+      }
+      request.filtros = { termo: term, localidade_id: $localidade_id_inicial.val() }
+      $.getJSON( BASE_URL + "/setor_comercial", request, function( data, status, xhr ) {
+        var result = $.map(data.entidades, function (value, key) {
+          return {
+            id: value.id,
+            label: value.nome,
+            value: value.nome,
+            codigo: value.codigo
+          };
+        });
+        cache[ term ] = result;
+        response( result );
+      });
+    },
+    select: function(event, ui){
+      $setor_comercial_id_inicial.val(ui.item.codigo);
+      $rota_inicial.attr("disabled", false);
+      carregarRotasInicial();
+    }
+  });
+
+  function carregarRotasInicial(callback){
+    $rota_inicial.empty();
+    var request = {};
+    $rota_inicial.append('<option value=""></option>')
+    request.filtros = { setor_comercial_id: $setor_comercial_id_inicial.val() };
+    $.getJSON( BASE_URL + "/rotas", request, function( data, status, xhr ) {
+      $.each(data.entidades, function(index, value) {
+          $rota_inicial.append('<option value="'+ value.codigo +'">'+ value.codigo +'</option>')
+      });
+      if (callback) callback();
+    });
+  }
+
+  var cache = {},
   $localidade_id_final = $("#localidade_id_final"),
   $setor_comercial = $("#setor_comercial_final"),
   $setor_comercial_id_final = $("#setor_comercial_id_final"),
@@ -93,7 +185,7 @@ $(function(){
 
     $.getJSON( BASE_URL + "/rotas", request, function( data, status, xhr ) {
       $.each(data.entidades, function(index, value) {
-          $rota_final.append('<option value="'+ value.id +'">'+ value.codigo +'</option>')
+          $rota_final.append('<option value="'+ value.codigo +'">'+ value.codigo +'</option>')
       });
 
       if (callback) callback();
