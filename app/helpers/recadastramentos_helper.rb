71501aff5100fb49aad9f774972e969e594db0f5
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-09-03 16:56:34 -0300

    Melhoria na pagina de pesquisa para o recadastramento para exibir mais situcoes para filtro

--- app/helpers/recadastramentos_helper.rb
+++ app/helpers/recadastramentos_helper.rb
@@ -2,4 +2,14 @@ module RecadastramentosHelper
   def qnt_colspan(em_revisao)
     em_revisao ? 7 : 6
   end
+
+  def exibir_imoveis_options
+    [["Pendentes", 3],
+     ["Em Revisão", 8],
+     ["Revisados", 9],
+     ["Pré-Aprovados", 7],
+     ["Pré-Aprovar em Lote", -2],
+     ["Todos", -1]
+    ]
+  end
 end
