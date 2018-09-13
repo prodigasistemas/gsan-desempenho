f4244f23a75eecdf127cd66aa01509ab224cf153
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-09-13 08:40:47 -0300

    Adicionado atributo indicador_validacao no model CadastroOcorrencia

--- app/models/cadastro_ocorrencia.rb
+++ app/models/cadastro_ocorrencia.rb
@@ -5,4 +5,16 @@ class CadastroOcorrencia < ClienteAPI::Model
                 :ultima_alteracao,
                 :indicador_suspende_contrato,
                 :indicador_validacao
+
+  def self.com_indicador_validacao(ocorrencias)
+    ocorrencias.select { |o| o.is_indicador_validacao? }
+  end
+
+  def self.sem_indicador_validacao(ocorrencias)
+    ocorrencias.select { |o| !o.is_indicador_validacao? }
+  end
+
+  def is_indicador_validacao?
+    indicador_validacao.present? and indicador_validacao == 1
+  end
 end
